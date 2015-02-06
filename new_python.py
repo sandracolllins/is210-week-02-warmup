360c28d51cab701680f0dd85eb3c662a2876e2f1
sandracollins <sandra.collins@spsmail.cuny.edu>
2015-02-06 12:51:19 +0000

    Wrote script to print changing variables

---
+++ new_python.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""First Python print variable script."""
+
+my_var = "blue house"
+my_new_var = "red truck"
+
+print my_var
+
+my_var = my_new_var
+
+print my_var
+
